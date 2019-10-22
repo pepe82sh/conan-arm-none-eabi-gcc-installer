f74eda4fa17be0abb38a5ed84f0561685276a165
pepe <35840364+pepe82sh@users.noreply.github.com>
2019-10-22 08:18:09 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class ConanFileInst(ConanFile):
         "8.2": (arm_common_path + "/8-2018q4", "gcc-arm-none-eabi-8-2018-q4-major-"),
         "7.3": (arm_common_path + "/7-2018q2", "gcc-arm-none-eabi-7-2018-q2-update-"),
         "7.2": (arm_common_path + "/7-2017q4", "gcc-arm-none-eabi-7-2017-q4-major-"),
-        "6.3": (arm_common_path + "/6_1-2017q2", "gcc-arm-none-eabi-6-2017-q2-update-"),
+        "6.3": (arm_common_path + "/6-2017q2", "gcc-arm-none-eabi-6-2017-q2-update-"),
         "6.2": (arm_common_path + "/6-2016q4", "gcc-arm-none-eabi-6_2-2016q4-20161216-"),
         "5.4": (arm_common_path + "/5_4-2016q3", "gcc-arm-none-eabi-5_4-2016q3-20160926-"),
     }
