a5f2eb9a2517d493253647f96f8b7930f08dadd8
pepe <35840364+pepe82sh@users.noreply.github.com>
2019-10-22 09:20:14 +0200

    Update conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,7 +8,10 @@ class ConanFileInst(conans.ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = conans.CMake(self)
+        generator = None
+        if self.settings.os == "Windows":
+            generator = "MinGW Makefiles"
+        cmake = conans.CMake(self, generator=generator)
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
         # in "test_package"
         cmake.configure()
